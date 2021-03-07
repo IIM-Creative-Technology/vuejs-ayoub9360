9ba6a484cd91aad3c456bdfa7e998155f6c65a1a
Ayoub <ayoub.el_guendouz@edu.Devinci.fr>
2021-03-07 23:26:28 +0100

    ADD ArticleItem components

---
+++ src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    <h1>{{ title }}</h1>
+    <div>
+      <label for="pseudo">Pseudo</label>
+      <input type="text" id="pseudo" placeholder="Pseudo">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  props: {
+    title: {
+      required: true,
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
