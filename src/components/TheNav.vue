35ee2c440144c255496b226f0f9265626961accf
Ayoub <ayoub.el_guendouz@edu.Devinci.fr>
2021-03-11 18:37:37 +0100

    ADD Nav

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,13 +1,43 @@
 <template>
-
+  <div>
+    <ul>
+      <li>Admin</li>
+      <li>Blog</li>
+      <li>Login</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-name: "TheNav"
+  name: 'TheNav',
+
 }
 </script>
 
-<style scoped>
+
+<style scoped lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+div {
+  border-radius: 10px;
+}
+
+ul {
+  background: linear-gradient(90deg, rgba(250, 150, 26, 1) 0%, rgba(255, 193, 12, 1) 100%);
+  display: flex;
+  flex-direction: row;
+  color: white;
+  list-style: none;
+
+  li {
+    padding: 15px;
+    font-weight: bold;
+  }
+}
 
 </style>
